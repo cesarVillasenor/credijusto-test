c401a56499d59c27a5522b4f290f57b42e227b2f
Cesar <cesar.villasenor.g@gmail.com>
2021-01-22 14:24:08 -0800

    (V 0.1) Update to handle statics on dev.

--- credijustotest/urls.py
+++ credijustotest/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -7,3 +9,6 @@ urlpatterns = [
     # path('api-auth/', include('rest_framework.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
