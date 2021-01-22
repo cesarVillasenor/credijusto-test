7fd6684db281a6e918bd1a7527b624888f1f7439
Cesar <cesar.villasenor.g@gmail.com>
2021-01-22 10:48:11 -0800

    (V 0.1) APIView Rateview created to return Banxico rate.

---
+++ apps/rates/api/viewsets.py
@@ -0,0 +1,31 @@
+import requests
+import json
+from django.conf import settings
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class RatesView(APIView):
+    renderer_classes = [JSONRenderer]
+    banxico_url = 'https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF43718/datos/oportuno?token='
+    banxico_url += settings.BANXICO_TOKEN
+
+    def get(self, request, format=None):
+        content = {'rates': {
+        }
+        }
+
+        banxico_request = requests.get(self.banxico_url)
+        bmx_content = json.loads(banxico_request.content)
+        bmx_data = bmx_content['bmx']['series'][0]['datos']
+        bmx_dict = {
+            'Banxico': {
+                'value': bmx_data[0]['dato'],
+                'last_updated': bmx_data[0]['fecha'],
+            },
+        }
+        content['rates'].update(bmx_dict)
+
+        return Response(content)
+
