bf7d97ca460066d93670d4315a697b56e67298be
Cesar <cesar.villasenor.g@gmail.com>
2021-01-22 14:57:23 -0800

    (V 0.1) Typo fixed and dependencies no used deleted.

--- apps/tokeninfo/models.py
+++ apps/tokeninfo/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 from rest_framework.authtoken.models import Token
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.utils.timezone import now, timedelta
 
 
 class TokenInfo(models.Model):
@@ -10,7 +7,7 @@ class TokenInfo(models.Model):
     limit_usage = models.IntegerField()
 
 
-class TokeUsage(models.Model):
+class TokenUsage(models.Model):
     token_info = models.ForeignKey(TokenInfo, on_delete=models.PROTECT)
     date = models.DateField()
 
