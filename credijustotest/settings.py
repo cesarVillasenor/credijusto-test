28ae566a5cfdb87d09f7785fbfe3d981a1df3862
Cesar <cesar.villasenor.g@gmail.com>
2021-01-22 16:36:43 -0800

    (V 0.1) Heroku configuration files fixed.

--- credijustotest/settings.py
+++ credijustotest/settings.py
@@ -106,7 +106,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+DATA_DIR = os.path.dirname(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
