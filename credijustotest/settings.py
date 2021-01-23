f86111bc0fa0099dafb6f50eceb933faa8d9a63b
Cesar <cesar.villasenor.g@gmail.com>
2021-01-22 16:40:39 -0800

    (V 0.1) Heroku configuration files fixed.

--- credijustotest/settings.py
+++ credijustotest/settings.py
@@ -108,9 +108,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 DATA_DIR = os.path.dirname(os.path.dirname(__file__))
 STATIC_ROOT = os.path.join(DATA_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'credijustotest', 'static'),
+# )
 
 #################
 # LOCAL SETTINGS #credijustotest
